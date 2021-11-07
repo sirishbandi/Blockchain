667db3afc12b0093752e12692cb51f4084d6fe1b
sirishbandi <30628377+sirishbandi@users.noreply.github.com>
2021-11-07 20:39:04 +0530

    Update types.go

--- types.go
+++ types.go
@@ -15,6 +15,7 @@ const (
 	HASH_MAX_TIME  = 5 * time.Minute
 	MAX_BACKLOG    = 5
 	PEER_SYNC_TIME = 1 * time.Minute
+	FIRST_BLOCK = "40198a17205aca94791e2425fda69eb0b6af99626fe17159c82426b2a14ca3653baa999a66d07dd977b58358607b99d546249f08b1448d6c4fd70389f6bf845e"
 )
 
 type Config struct {
